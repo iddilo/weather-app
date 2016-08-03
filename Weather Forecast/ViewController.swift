7553f49e07c9bdf991879f8f7241dfd3579a614a
p.mitev <pmitev@pmitevs-MacBook.local>
2016-08-03 20:16:15 +0300

    Initial commit

---
+++ Weather Forecast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather Forecast
+//
+//  Created by p.mitev on 07.03.16.
+//  Copyright © 2016 p.mitev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
